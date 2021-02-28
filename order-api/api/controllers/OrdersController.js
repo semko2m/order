f5b01fd74d0727b19c7323202b2e66c3765bf88e
Semir Hodzic <semko2m@gmail.com>
2021-02-28 17:05:14 +0100

    s

--- order-api/api/controllers/OrdersController.js
+++ order-api/api/controllers/OrdersController.js
@@ -64,35 +64,5 @@ module.exports = {
       });
     }
   }
-
-
-  // /**
-  //  * find
-  //  * @param req
-  //  * @param res
-  //  * @returns {Promise<*|Promise<any>>}
-  //  */
-  // search: async function (req, res) {
-  //   sails.log.info(req.query);
-  //   if (req.query.startDate && req.query.endDate) {
-  //     let users;
-  //     try {
-  //       users = await Users.find({
-  //         createdAt: {'>': new Date(req.query.startDate), '<': new Date(req.query.endDate)}
-  //       });
-  //       return res.json(users);
-  //     } catch (e) {
-  //       return res.json({
-  //         Error: 'Is not valid'
-  //       });
-  //     }
-  //
-  //   } else {
-  //     return res.json({
-  //       Error: 'Start and end date is not provided'
-  //     });
-  //   }
-  //
-  // }
 };
 
